b31f8b3c7293a063c5927a1c821ad76fa5f2206d
Võ Luyện <voluyenpr@gmail.com>
2026-09-13 15:20:29 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,8 +1,8 @@
-#1 +10
+#2 -0
 #spectral_guided_learning
 #v2
 # nvidia-smi
-cd ./SpectralGuidedLearning && bash ./project_commands.sh
+# cd ./SpectralGuidedLearning && bash ./project_commands.sh
 # cd ./SegmentSelectiveSFT
 # bash ./commands.sh
 #download_sdxl_q3_offline_assets
