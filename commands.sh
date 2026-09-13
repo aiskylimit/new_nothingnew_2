2d56dc2b682b1ca49778a2ee8858f17f23263805
phongdq25 <nothing>
2026-09-13 21:11:45 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#1 +60
+#2
 #offline_rlsd_sdpo_b200
 #v1
 # nvidia-smi
@@ -56,5 +56,5 @@ export NCCL_DEBUG=WARN
 # cd ./SegmentSelectiveSFT
 # CUDA_VISIBLE_DEVICES=0,1 bash commands.sh
 
-cd ./offline_rlsd_sdpo_b200
-bash ./project_commands.sh
+# cd ./offline_rlsd_sdpo_b200
+# bash ./project_commands.sh
