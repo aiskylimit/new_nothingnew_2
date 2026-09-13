1b9fef7279b9f509b11a62fe0fef0bbd17d155b1
Võ Luyện <voluyenpr@gmail.com>
2026-09-13 11:24:27 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,11 +1,11 @@
-#1 +10
+#2 -0
 #ssft
 #v1
-kill -9 114651
+# kill -9 114651
 nvidia-smi
 # cd ./SpectralGuidedLearning && bash ./project_commands.sh
-cd ./SegmentSelectiveSFT
-bash ./commands.sh
+# cd ./SegmentSelectiveSFT
+# bash ./commands.sh
 #download_sdxl_q3_offline_assets
 
 # The 10 spectral_guided_learning items above are kept intact and first in the
