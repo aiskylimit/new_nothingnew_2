3a97a7413dce11af4390d80f5f510da92b6f9e5e
phongdq25 <nothing>
2026-09-13 23:28:21 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#1 +30
+#1 +10
 #offline_rlsd_sdpo_b200
 #v1
 
@@ -59,4 +59,7 @@ export NCCL_DEBUG=WARN
 
 cd ./offline_rlsd_sdpo_b200
 # tail -n 30 results_rlsd_4b_dryrun_train.log
-bash ./project_commands.sh
+# bash ./project_commands.sh
+source /mnt/local/uvenvs/tropic/bin/activate
+which vllm
+pip show vllm
