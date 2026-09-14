b2a692764e67b04c0b1ccd76fe77d1013f96fd2a
phongdq25 <nothing>
2026-09-14 11:18:44 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
-#2 -0-10
-#opsd
+#1 +60
+#offline_rlsd_sdpo_b200
 #v1
 
 # cd SegmentSelectiveSFT/Attribution/processed_data/s1k && find s1k -type f -exec du -h {} + | sort -hr
@@ -58,10 +58,9 @@ export NCCL_DEBUG=WARN
 # CUDA_VISIBLE_DEVICES=0,1 bash commands.sh
 
 
-# cd ./offline_rlsd_sdpo_b200
+cd ./offline_rlsd_sdpo_b200
 # tail -n 30 results_rlsd_4b_dryrun_train.log
-# cat /mnt/local/aiskylimit_new_nothingnew_2/offline_rlsd_sdpo_b200/results_rlsd_4b_dryrun/vllm_replica_gpu3.log
-# bash ./project_commands.sh
+bash ./project_commands.sh
 
 # cd ./opsd
 # bash ./project_commands.sh
