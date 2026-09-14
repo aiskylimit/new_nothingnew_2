d8a0a44b2ca6a90b1f90b3eda8e0d8f9b79ff704
Dat Dao <111187020+dvtiendat@users.noreply.github.com>
2026-09-14 11:27:13 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
-#2
-#offline_rlsd_sdpo_b200
+#2 -0-5
+#opsd
 #v1
 
 # cd SegmentSelectiveSFT/Attribution/processed_data/s1k && find s1k -type f -exec du -h {} + | sort -hr
@@ -58,9 +58,9 @@ export NCCL_DEBUG=WARN
 # CUDA_VISIBLE_DEVICES=0,1 bash commands.sh
 
 
-cd ./offline_rlsd_sdpo_b200
+# cd ./offline_rlsd_sdpo_b200
 # tail -n 30 results_rlsd_4b_dryrun_train.log
-bash ./project_commands.sh
+# bash ./project_commands.sh
 
 # cd ./opsd
 # bash ./project_commands.sh
