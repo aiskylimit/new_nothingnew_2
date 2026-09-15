94989395a0af6d9a5196e32158362d2caca6af3c
Dat Dao <111187020+dvtiendat@users.noreply.github.com>
2026-09-15 14:33:23 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,11 +1,11 @@
-#1 +60
-#ssft_full
+#2 -0-15
+#opsd
 #v1
 
 # cd ./P-ALIGN
 # CUDA_VISIBLE_DEVICES=1 bash ./project_commands.sh
 # cd ./SpectralGuidedLearning && bash ./project_commands.sh
-cd ./SegmentSelectiveSFT && bash ./commands.sh
+# cd ./SegmentSelectiveSFT && bash ./commands.sh
 
 #download_sdxl_q3_offline_assets
 
