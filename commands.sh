f1d32a17752fb01812f3f316fc31656991aaece9
voluyen <voluyenpr@gmail.com>
2026-09-15 11:36:33 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,9 +1,9 @@
-#1 +60
-#palign
+#2 -1
+#ssft_full
 #v1
 
-cd ./P-ALIGN
-CUDA_VISIBLE_DEVICES=1 bash ./project_commands.sh
+# cd ./P-ALIGN
+# CUDA_VISIBLE_DEVICES=1 bash ./project_commands.sh
 # cd ./SpectralGuidedLearning && bash ./project_commands.sh
 # cd ./SegmentSelectiveSFT && bash ./commands.sh
 
