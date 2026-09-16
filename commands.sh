642a940e109ae28037f721e2da76a0c1d45cebee
voluyen <voluyenpr@gmail.com>
2026-09-16 11:31:28 +0700

    rerun palign

--- commands.sh
+++ commands.sh
@@ -1,9 +1,9 @@
-#2 -0
+#1 +1800
 #palign
 #v1
 
-# cd P-ALIGN
-# CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
+cd P-ALIGN
+CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
 
 # nvidia-smi
 # SDXL_ENV=/mnt/local/uvenvs/sdxl-q3-offline-b200-2gpu
