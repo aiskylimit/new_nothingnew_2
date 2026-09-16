dbe876f39a1d68b9891ee7323b097456a872f7b9
voluyen <voluyenpr@gmail.com>
2026-09-16 13:17:02 +0700

    get result palign

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
-#1 +150
-#opsd
+#2 -f-/mnt/local/aiskylimit_new_nothingnew_2/P-ALIGN/output/eval_results.txt
+#palign
 #v1
 
 # cd P-ALIGN2
@@ -55,5 +55,5 @@ export NCCL_DEBUG=WARN
 # cd ./offline_rlsd_sdpo_b200
 # bash project_commands.sh
 
-cd ./opsd
-bash project_commands.sh
+# cd ./opsd
+# bash project_commands.sh
