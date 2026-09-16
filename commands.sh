4abb08b219dc42d6ba04520b9e1cb346b949718d
voluyen <voluyenpr@gmail.com>
2026-09-16 16:33:51 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 -f-/mnt/local/aiskylimit_new_nothingnew_2/P-ALIGN/output/eval_results.txt
+#1 +60
 #palign
 #v1
 
@@ -8,8 +8,8 @@
 #palign
 #v1
 
-# cd P-ALIGN
-# CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
+cd P-ALIGN
+CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
 
 nvidia-smi
 # SDXL_ENV=/mnt/local/uvenvs/sdxl-q3-offline-b200-2gpu
