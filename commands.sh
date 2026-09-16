cea665d0ff09e01a23984275faeea60719620ac4
danhvinhle-zenai <danhvinhle.zenai@gmail.com>
2026-09-17 00:27:48 +0700

    update

--- commands.sh
+++ commands.sh
@@ -29,9 +29,9 @@
 #v1
 
 # cd P-ALIGN
-nvidia-smi
-cd ./OPSD
-tar -czvf opsd_4b_results.tar.gz results/raw/qwen3-4b/opsd
+# nvidia-smi
+# cd ./OPSD
+# tar -czvf opsd_4b_results.tar.gz results/raw/qwen3-4b/opsd
 # CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
 
 # nvidia-smi
