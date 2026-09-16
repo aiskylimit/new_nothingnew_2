dde599f6cede31ecf38123c8de464d46546b99d5
voluyen <voluyenpr@gmail.com>
2026-09-16 14:41:56 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,9 +1,9 @@
-#2 -1
+#1 +60
 #palign
 #v1
 
-# cd P-ALIGN
-# CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
+cd P-ALIGN
+CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
 
 nvidia-smi
 # SDXL_ENV=/mnt/local/uvenvs/sdxl-q3-offline-b200-2gpu
