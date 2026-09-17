e7b60859159c18824cfe6e4e6e7531d885ff0d01
voluyen <voluyenpr@gmail.com>
2026-09-17 09:55:53 +0700

    set up sft

--- commands.sh
+++ commands.sh
@@ -1,10 +1,10 @@
-#1 +60
+#i spectral_guided_learning_train.txt
 #palign
 #v1
 
-cd P-ALIGN
-nvidia-smi
-CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
+# cd P-ALIGN
+# nvidia-smi
+# CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
 
 nvidia-smi
 # SDXL_ENV=/mnt/local/uvenvs/sdxl-q3-offline-b200-2gpu
