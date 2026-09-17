07e83e8a933631e5dcf6906657df26e21778854e
Dat Dao <111187020+dvtiendat@users.noreply.github.com>
2026-09-17 15:38:46 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,11 +1,11 @@
-#1 +60
-#sft
-#v2
+#2 -f-/mnt/local/aiskylimit_new_nothingnew_2/offline_rlsd_sdpo_b200/results_rlsd_4b_eval_step75.log
+#tropic
+#v1
 
 # cd P-ALIGN
 # # nvidia-smi
 # CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
-cd SpectralGuidedLearning && GPUS=0 bash project_commands.sh
+# cd SpectralGuidedLearning && GPUS=0 bash project_commands.sh
 
 nvidia-smi
 
