cf0af806a225cd8a5fe5f2d4e30f5797028eb916
Dat Dao <111187020+dvtiendat@users.noreply.github.com>
2026-09-17 15:59:39 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 -f-/mnt/local/aiskylimit_new_nothingnew_2/offline_rlsd_sdpo_b200/results_rlsd_8b_eval_step75.log +a
+#2 -f-/mnt/local/aiskylimit_new_nothingnew_2/offline_rlsd_sdpo_b200/results_rlsd_8b_eval_step100.log +a
 #tropic
 #v1
 
