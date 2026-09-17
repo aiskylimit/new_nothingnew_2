bf1a393f6e2a55ec3fe2f3f62571b7f78dcbbcec
DanhVinhLe <danhvinhlee@gmail.com>
2026-09-17 11:07:22 +0700

    update

--- VLM_Distillation-main/project_commands.sh
+++ VLM_Distillation-main/project_commands.sh
@@ -20,16 +20,16 @@ fi
 
 # Training uses the relative IMAGE_DIR=train_data.  The JSON image values start
 # # with coco/, gqa/, ocr_vqa/, textvqa/, and vg/.
-unzip -q -o "${DOWNLOAD_DATA_DIR}/coco/train2017.zip" -d "${DATA_DIR}/coco"
-unzip -q -o "${DOWNLOAD_DATA_DIR}/gqa/images.zip" -d "${DATA_DIR}/gqa"
-unzip -q -o "${DOWNLOAD_DATA_DIR}/textvqa/train_val_images.zip" -d "${DATA_DIR}/textvqa"
-unzip -q -o "${DOWNLOAD_DATA_DIR}/ocr_vqa/ocr_vqa_images.zip" -d "${DATA_DIR}/ocr_vqa"
-unzip -q -o "${DOWNLOAD_DATA_DIR}/vg/images.zip" -d "${DATA_DIR}/vg"
-unzip -q -o "${DOWNLOAD_DATA_DIR}/vg/images2.zip" -d "${DATA_DIR}/vg"
+# unzip -q -o "${DOWNLOAD_DATA_DIR}/coco/train2017.zip" -d "${DATA_DIR}/coco"
+# unzip -q -o "${DOWNLOAD_DATA_DIR}/gqa/images.zip" -d "${DATA_DIR}/gqa"
+# unzip -q -o "${DOWNLOAD_DATA_DIR}/textvqa/train_val_images.zip" -d "${DATA_DIR}/textvqa"
+# unzip -q -o "${DOWNLOAD_DATA_DIR}/ocr_vqa/ocr_vqa_images.zip" -d "${DATA_DIR}/ocr_vqa"
+# unzip -q -o "${DOWNLOAD_DATA_DIR}/vg/images.zip" -d "${DATA_DIR}/vg"
+# unzip -q -o "${DOWNLOAD_DATA_DIR}/vg/images2.zip" -d "${DATA_DIR}/vg"
 
 #bash download_datatrain.sh
 
 export CUDA_VISIBLE_DEVICES=6,7
-# bash script_train/run_baseline.sh
+bash script_train/run_baseline.sh
 
-bash script_train/run_propose.sh
+# bash script_train/run_propose.sh
