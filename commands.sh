9b63734e7b8cdc6133c504d2b738dbeac93afc40
voluyen <voluyenpr@gmail.com>
2026-09-17 16:48:15 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,11 +1,11 @@
-#1 +60
+#2 -0-10
 #eval_sft
 #v1
 
 # cd P-ALIGN
 # # nvidia-smi
 # CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
-cd SpectralGuidedLearning && GPUS=0 bash project_commands.sh
+# cd SpectralGuidedLearning && GPUS=0 bash project_commands.sh
 
 nvidia-smi
 
