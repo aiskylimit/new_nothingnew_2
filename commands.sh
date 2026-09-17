ee45b6c2ea36d11aac21300623d34065e213ac97
Dat Dao <111187020+dvtiendat@users.noreply.github.com>
2026-09-17 10:43:24 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,12 +1,12 @@
-#2 -0-10
-#sft
+#2 -f-/mnt/local/aiskylimit_new_nothingnew_2/OPSD/opsd_4b_results_done_v5.zip.part-aa
+#opsd
 #v1
 
-# cd SpectralGuidedLearning
-# # cd P-ALIGN
-# nvidia-smi
-# CUDA_VISIBLE_DEVICES=0 bash project_commands.sh
+# cd P-ALIGN
+# # nvidia-smi
 
+
+# CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
 
 nvidia-smi
 # SDXL_ENV=/mnt/local/uvenvs/sdxl-q3-offline-b200-2gpu
@@ -63,7 +63,3 @@ export NCCL_DEBUG=WARN
 
 # cd ./VLM_Distillation-main
 # bash project_commands.sh
-
-# cd ./OPSD
-# # python -c "import zipfile, os; folder='results/raw/qwen3-4b/opsd'; output='opsd_4b_results_done_v5.zip'; z=zipfile.ZipFile(output, 'w', zipfile.ZIP_DEFLATED); [z.write(os.path.join(root,f), os.path.relpath(os.path.join(root,f), os.path.dirname(folder))) for root,_,files in os.walk(folder) for f in files]; z.close()" && echo "=== ZIP DONE ===" && ls -lh opsd_4b_results_done_v5.zip
-# split -b 24M opsd_4b_results_done_v5.zip opsd_4b_results_done_v5.zip.part- && echo "=== SPLIT DONE ===" && ls -lh opsd_4b_results_done_v5.zip.part-*
