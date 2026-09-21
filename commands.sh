d3e633ead85e7c0f305f730a0586ac5ae68a6ced
Võ Luyện <voluyenpr@gmail.com>
2026-09-21 23:23:45 +0700

    download for ssft

--- commands.sh
+++ commands.sh
@@ -1,4 +1,15 @@
-#i ssft_train.txt
+#d
+#datasets
+--hf-dataset baesad/s1K-1.1-deepseek-cot /mnt/local/_data/@PROJECT@/s1k
+--hf-dataset math-ai/aime24 /mnt/local/_data/@PROJECT@/aime24
+--hf-dataset math-ai/aime25 /mnt/local/_data/@PROJECT@/aime25
+--hf-dataset HuggingFaceH4/MATH-500 /mnt/local/_data/@PROJECT@/MATH-500
+--hf-dataset AI-MO/aimo-validation-amc /mnt/local/_data/@PROJECT@/aimo-validation-amc
+#models
+--hf Qwen/Qwen2.5-7B-Instruct /mnt/local/_models/@PROJECT@/Qwen2.5-7B-Instruct
+--hf Qwen/Qwen3-8B /mnt/local/_models/@PROJECT@/Qwen3-8B
+--hf deepseek-ai/DeepSeek-R1-Distill-Qwen-7B /mnt/local/_models/@PROJECT@/DeepSeek-R1-Distill-Qwen-7B
+
 #VLM_Distillation
 #v1
 
