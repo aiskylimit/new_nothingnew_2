e2517132f4110847f3afc236387ef62e1510bce2
voluyen <voluyenpr@gmail.com>
2026-09-22 10:04:06 +0700

    update

--- commands_backup.sh
+++ commands_backup.sh
@@ -2,7 +2,7 @@
 #ssft_8b
 #v2
 
-#2 -f-/mnt/local/aiskylimit_new_nothingnew2/SegmentSelectiveSFT
+#2 -f-/mnt/local/aiskylimit_new_nothingnew_2/SegmentSelectiveSFT
 #2 -f-/mnt/local/aiskylimit_new_nothing/talas_vlm_embed/MMEB-evaloutputs-json-v3/ +a
 #2 -f-/mnt/local/aiskylimit_new_nothing/_run_log_/_run-2026-09-03_17-01-16-VLM-Distillation.log
 
