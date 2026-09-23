17f3c3878fde33392d41f27f83b7a5aa4c81865c
Võ Luyện <voluyenpr@gmail.com>
2026-09-24 00:03:10 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,9 +1,9 @@
-#1 +5
+#1 +30
 #p-align
 #v1
 
 # cd SegmentSelectiveSFT && bash commands.sh
-nvidia-smi
+cd P-ALIGN && CUDA_VISIBLE_DEVICES=1 bash project_commands_r1_1.5b.sh
 # cd SegmentSelectiveSFT && GPU=0 bash commands.sh
 # CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
 # cd ./offline_olmo7b_b200
