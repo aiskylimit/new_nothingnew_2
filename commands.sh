73b4068d194ca8d3b0bfef27a010ecdc5124a625
phongdq25 <nothing>
2026-09-23 14:41:47 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,10 @@
-#1 +10
+#d
+#datasets
+--url https://huggingface.co/datasets/VoCuc/UltraInteract-Infer/resolve/main/google/gemma-2-9b-it/generated_train.jsonl /mnt/local/aiskylimit_new_nothing/multi-mode-distill/data/raw/google/gemma-2-9b-it/
+#models
+--hf google/gemma-2-9b-it /mnt/local/aiskylimit_new_nothing/multi-mode-distill/models/google_gemma-2-9b-it
+--hf google/gemma-2-2b-it /mnt/local/aiskylimit_new_nothing/multi-mode-distill/models/google_gemma-2-2b-it
+
 #spectral
 #v1
 
@@ -7,7 +13,7 @@
 # CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
 # cd ./offline_olmo7b_b200
 # bash project_commands.sh
-cd SpectralGuidedLearning && GPUS=1 bash project_commands_r1-qwen-1.5b.sh
+# cd SpectralGuidedLearning && GPUS=1 bash project_commands_r1-qwen-1.5b.sh
 # kill -9 155157 155158
 
 # cd ./offline_rlsd_sdpo_b200
