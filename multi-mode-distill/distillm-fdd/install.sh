6273273b59b4359d22423eb1507472d0c6b5f331
giangdt172 <giangggiang111@gmail.com>
2026-09-23 22:34:00 +0700

    update mta

---
+++ multi-mode-distill/distillm-fdd/install.sh
@@ -0,0 +1,15 @@
+export NCCL_DEBUG=""
+pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu124
+pip install transformers==4.52.4
+# pip install vllm==0.5.0
+pip install deepspeed
+pip install nltk
+pip install numerize
+pip install rouge-score
+pip install torchtyping
+pip install rich
+pip install accelerate
+pip install datasets
+pip install sentencepiece
+pip install protobuf
+pip install peft
