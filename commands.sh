32c690d919c2780b1b21de8139ba62a855eabe89
phongdq25 <nothing>
2026-09-24 23:17:55 +0700

    update

--- commands.sh
+++ commands.sh
@@ -2,6 +2,9 @@
 #test
 #v2
 
+kill -9 499 500 501 502 503 504 505 506
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python ./talas_vlm_embed/multi_gpu_v2.py
+
 nvidia-smi
 
 # cd SegmentSelectiveSFT && bash commands.sh
@@ -11,7 +14,6 @@ nvidia-smi
 # cd ./offline_olmo7b_b200
 # bash project_commands.sh
 # cd P-ALIGN && GPUS=1 bash  project_commands_r1_1.5b.sh
-# kill -9 155157 155158
 # cd ./SpectralGuidedLearning && GPUS=0 bash project_commands_answer_r1-qwen-1.5b.sh 
 # cd ./offline_rlsd_sdpo_b200
 # ls
