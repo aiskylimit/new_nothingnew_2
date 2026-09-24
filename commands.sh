1421da614e4293c8f32a9d74ac34f0b60d5e651d
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-09-24 22:32:10 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -54,8 +54,9 @@ export NCCL_DEBUG=WARN
 
 cd ./multi-mode-distill
 # bash ./run_gemma.sh
-tree /mnt/local/aiskylimit_new_nothingnew_2/multi-mode-distill/results/gemma-2-2b-it-distill/geo1_cka0_menger0.0/e2-bs8-lr0.0001-G4-N4-NN1-kd0.5-lora-16-128-0.05/evaluation_gemma
+# tree /mnt/local/aiskylimit_new_nothingnew_2/multi-mode-distill/results/gemma-2-2b-it-distill/geo1_cka0_menger0.0/e2-bs8-lr0.0001-G4-N4-NN1-kd0.5-lora-16-128-0.05/evaluation_gemma
 # bash ./project_commands.sh
 # # # bash ./project_commands_opsd_ablation.sh
 # # # bash ./project_commands_ablation.sh
 # cat /mnt/local/aiskylimit_new_nothingnew_2/multi-mode-distill/results/gemma-2-2b-it-distill/geo1_cka0_menger0.0/e2-bs8-lr0.0001-G4-N4-NN1-kd0.5-lora-16-128-0.05/evaluation_gemma/code/__mnt__local__aiskylimit_new_nothing__reasoning_velocity_distill__models__google_gemma-2-2b-it/samples_mbpp_2026-09-24T07-21-52.951985.jsonl
+cat /mnt/local/aiskylimit_new_nothingnew_2/multi-mode-distill/results/gemma-2-2b-it-distill/geo1_cka0_menger0.0/e2-bs8-lr0.0001-G4-N4-NN1-kd0.5-lora-16-128-0.05/evaluation_gemma/code/__mnt__local__aiskylimit_new_nothing__reasoning_velocity_distill__models__google_gemma-2-2b-it/samples_mbpp_2026-09-24T14-54-48.288245.jsonl
