beae40d6c76d7936a76ba53d30ead7768f756a22
phongdq25 <nothing>
2026-09-24 14:56:19 +0700

    update

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-#2 -0-10
+#2 -f-/mnt/local/aiskylimit_new_nothingnew_2/multi-mode-distill/results/gemma-2-2b-it-distill/geo1_cka0_menger0.0/e2-bs8-lr0.0001-G4-N4-NN1-kd0.5-lora-16-128-0.05/evaluation_gemma/code/__mnt__local__aiskylimit_new_nothing__reasoning_velocity_distill__models__google_gemma-2-2b-it/
 #reeval
 #v1
 
