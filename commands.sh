d4aa7a42c08f8e7c1d7b5ad9e0a93a1fa3cb7fe2
aiskylimit <nguyenthienhuu88bk@gmail.com>
2026-09-24 15:19:56 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,10 +1,10 @@
-#2 -0-10
-#reeval
+#1 +10
+#test
 #v1
 
 # cd SegmentSelectiveSFT && bash commands.sh
 # cd P-ALIGN && CUDA_VISIBLE_DEVICES=1 bash project_commands_r1_1.5b.sh
-cd SegmentSelectiveSFT && GPU=0 bash commands.sh
+# cd SegmentSelectiveSFT && GPU=0 bash commands.sh
 # CUDA_VISIBLE_DEVICES=1 bash project_commands.sh
 # cd ./offline_olmo7b_b200
 # bash project_commands.sh
@@ -57,4 +57,4 @@ export NCCL_DEBUG=WARN
 # bash ./project_commands.sh
 # # # bash ./project_commands_opsd_ablation.sh
 # # # bash ./project_commands_ablation.sh
-
+ls /mnt/local/aiskylimit_new_nothingnew_2/multi-mode-distill/results/gemma-2-2b-it-distill/geo1_cka0_menger0.0/e2-bs8-lr0.0001-G4-N4-NN1-kd0.5-lora-16-128-0.05/evaluation_gemma
